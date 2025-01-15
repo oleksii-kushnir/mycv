e7cde2ec54efb896c1ae92ac1590abe611ab2940
Alexey Kushnir <alexey.kushnir@office1.bg>
2025-01-15 10:01:54 +0200

    section 9

---
+++ src/users/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  LogInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+  @AfterUpdate()
+  LogUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  LogRemove() {
+    console.log('Removed User with id', this.id);
+  }
+}
